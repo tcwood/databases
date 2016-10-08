74efa02e9dec7800ebbffc23f4ca5405c68ac063
tcwood <travis.cwood@gmail.com>
2016-10-07 18:48:57 -0700

    Create basic database

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,15 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'chat'
+});
 
+con.connect(function(err) {
+  if (err) { return console.log(err); }
+
+  console.log('connection established');
+});
--- server/schema.sql
+++ server/schema.sql
@@ -2,16 +2,43 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+-- DROP TABLE IF EXISTS `Users`;
+    
+CREATE TABLE `Users` (
+  `username` VARCHAR(100) NULL DEFAULT NULL,
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
-/* Create other tables and define schemas for them here! */
+-- DROP TABLE IF EXISTS `Messages`;
+    
+CREATE TABLE `Messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `created-at` DATETIME(6) NULL DEFAULT NULL,
+  `messages` VARCHAR(500) NULL DEFAULT NULL,
+  `id_Users` INTEGER NULL DEFAULT NULL,
+  `id_Room` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
 
+-- DROP TABLE IF EXISTS `Room`;
+    
+CREATE TABLE `Room` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `roomname` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
 
+ALTER TABLE `Messages` ADD FOREIGN KEY (id_Users) REFERENCES `Users` (`id`);
+ALTER TABLE `Messages` ADD FOREIGN KEY (id_Room) REFERENCES `Room` (`id`);
 
+-- ---
+-- Test Data
+-- ---
 
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
-
+-- INSERT INTO `Users` (`username`,`id`) VALUES
+-- ('','');
+-- INSERT INTO `Messages` (`id`,`created-at`,`messages`,`id_Users`,`id_Room`) VALUES
+-- ('','','','','');
+-- INSERT INTO `Room` (`id`,`roomname`) VALUES
+-- ('','');
